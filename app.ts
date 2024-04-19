9638a387858270ee9d2920cad6d383d1ab52d4ca
Mahnoor604 <161150199+Mahnoor604@users.noreply.github.com>
2024-04-19 23:20:03 +0500

    APP.TS
    Guessing Number Game

---
+++ app.ts
@@ -0,0 +1,16 @@
+console.log("guess a number between 1 to 10");
+//now variable create
+let a:number=Math.floor(Math.random()*10);
+import inquirer from "inquirer"
+while(true){
+    let input =await inquirer.prompt
+   ({name: "guess" , type:"number" ,
+message: "Enter your guess number you want between 1 to 8:"})
+
+//provide code
+let ans:number = input.guess
+if (a==ans) 
+{console.log("Congraculation your guess number is correcrt")
+ break;}
+ else {console.log ("sorry your guess number wrong try again")}
+}
